8c2ae5ecf9f1233fe565a3d14c0853e70749d963
Rasinzz <88259422+Rasinzz@users.noreply.github.com>
2023-06-16 20:28:48 -0400

    Update `serverFiles` link

--- config.ps1
+++ config.ps1
@@ -1,7 +1,7 @@
 $downloadLinks = @{
     serverArtifacts = 'https://runtime.fivem.net/artifacts/fivem/build_server_windows/master/5848-4f71128ee48b07026d6d7229a60ebc5f40f2b9db/server.7z'
     serverData      = 'https://github.com/citizenfx/cfx-server-data/archive/refs/heads/master.zip'
-    serverFiles     = 'https://github.com/Rasinzz/fivem-server-setup-files-public/archive/heads/main.zip'
+    serverFiles     = 'https://github.com/Rasinzz/fivem-server-setup-gui-files/archive/heads/main.zip'
     vMenu           = 'https://github.com/TomGrobbe/vMenu/releases/download/v3.5.0/vMenu-v3.5.0.zip'
 }
 
